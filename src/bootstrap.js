e1318424cbacdb8a945c38f4b6ceaf0f95841319
Esperanza <espetomeiglesias@gmail.com>
2023-12-05 19:24:14 +0100

    babel-core-bootstrap

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,7 +7,9 @@ import App from "./components/app";
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
+var babel = require("@babel/core");
+import { transform } from "@babel/core";
+import * as babel from "@babel/core";
 import "./style/main.scss";
 
 function main() {
--- src/vendor.js
+++ src/vendor.js
@@ -5,6 +5,3 @@
 if (!window._babelPolyfill) {
   require('@babel/polyfill');
 }
-if(!window._babelCore){
-  require('@babel/core');
-}
